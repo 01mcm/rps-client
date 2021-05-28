12f7870faf87ca679cab1db19143d33e8f9a8d16
me <mclaudiamontalvo@gmail.com>
2021-05-28 11:33:31 +0200

    LoginAndGame

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '../views/Login'
+import Game from '../views/Game'
 
-import Screen1 from '../views/Screen1'
-import Screen2 from '../views/Screen2'
-import Screen3 from '../views/Screen3'
 
 Vue.use(VueRouter)
 
 const routes = [
-    {
-        path: '/',
-        name: 'home',
-        component: Screen1
-    },
-    {
-        path: '/screen1',
-        name: 'screen1',
-        component: Screen1
-    },
-    {
-        path: '/screen2',
-        name: 'screen2',
-        component: Screen2
-    },
-    {
-        path: '/screen3',
-        name: 'screen3',
-        component: Screen3
-    }
+   {
+       path:'/login',
+       name:'login',
+       component: Login
+   },
+   {
+    path:'/game',
+    name:'game',
+    component: Game
+}
 ]
 
 const router = new VueRouter({
